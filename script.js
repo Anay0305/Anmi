2d7b3e3dfe0e7b832a2593b11fd15d5c3773c7b0
Anay Gupta <100482615+Anay0305@users.noreply.github.com>
2024-02-13 23:50:38 +0530

    s

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ const highlightMenu = () => {
     homeMenu.classList.remove("highlight");
     resumeMenu.classList.remove("highlight");
     return;
-  } else if (window.innerWidth > 960 && scrollPos < 9800) {
+  } else if (window.innerWidth > 960 && scrollPos < 9700) {
     resumeMenu.classList.add("highlight");
     aboutMenu.classList.remove("highlight");
     stackMenu.classList.remove("highlight");
